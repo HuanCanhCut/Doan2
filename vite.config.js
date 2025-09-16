c9b0771aff7d6e79e7124baa157c26c671da162f
HuanCanhCut <tronghuanxxx@gmail.com>
2025-09-16 19:24:33 +0700

    chore: update package.json dependencies and enhance Vite configuration with new modal inputs and server settings

--- vite.config.js
+++ vite.config.js
@@ -22,7 +22,12 @@ export default defineConfig({
             input: {
                 main: 'index.html',
                 default: 'default.html',
+                login_modal: '/src/modal/login_modal.html',
+                register_modal: '/src/modal/register_modal.html',
             },
         },
+    },
+    server: {
+        allowedHosts: ['localhost', '127.0.0.1', 'doan.huancanhcut.click'],
     },
 })
