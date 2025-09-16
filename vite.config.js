30747f17aeb696a4a61d63005e3b8dd5a95f061e
HuanCanhCut <tronghuanxxx@gmail.com>
2025-09-16 18:47:29 +0700

    refactor: config vite build

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,28 @@
 // vite.config.js
-import {
-  defineConfig
-} from 'vite';
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-  plugins: [{
-    name: 'reload',
-    configureServer(server) {
-      const {
-        ws,
-        watcher
-      } = server;
-      watcher.on('change', file => {
-        if (file.endsWith('.html')) {
-          ws.send({
-            type: 'full-reload',
-          });
-        }
-      });
+    plugins: [
+        {
+            name: 'reload',
+            configureServer(server) {
+                const { ws, watcher } = server
+                watcher.on('change', (file) => {
+                    if (file.endsWith('.html')) {
+                        ws.send({
+                            type: 'full-reload',
+                        })
+                    }
+                })
+            },
+        },
+    ],
+    build: {
+        rollupOptions: {
+            input: {
+                main: 'index.html',
+                default: 'default.html',
+            },
+        },
     },
-  }, ],
-});
+})
