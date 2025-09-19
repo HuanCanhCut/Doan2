d9c60a0d95b14b421cf0343b2145f7f1ab57ae45
HuanCanhCut <tronghuanxxx@gmail.com>
2025-09-19 19:20:08 +0700

    feat: enhance image upload functionality with preview

--- src/js/helpers/getParentElement.js
+++ src/js/helpers/getParentElement.js
@@ -1,4 +1,8 @@
 const getParentElement = (element, selector) => {
+    if (element.matches(selector)) {
+        return element
+    }
+
     while (element.parentElement) {
         if (element.parentElement.matches(selector)) {
             return element.parentElement
--- src/js/post.js
+++ src/js/post.js
@@ -5,11 +5,14 @@ import locationsDropdownApp from './locationsDropdown'
 
 const categoryBtnsOption = document.querySelectorAll('.post__form__radio')
 const roleBtnsOption = document.querySelectorAll('.post__form__role')
+const imgInput = document.querySelector('#post__images')
+const imgUploadAreaPreview = document.querySelector('.post__images--upload--area--preview')
 
 const postApp = {
     locations: null,
     categoryType: 'sell', // sell or rent
     roleType: 'seller', // seller or agent
+    imagesFiles: [],
 
     handleValidator() {
         Validator({
@@ -55,6 +58,40 @@ const postApp = {
         }
     },
 
+    handleLoadImagesPreview() {
+        const uploadArea = document.querySelector('.post__images--upload--area:not(.uploaded)')
+        uploadArea.style.display = this.imagesFiles.length > 0 ? 'none' : 'flex'
+
+        imgUploadAreaPreview.style.display = this.imagesFiles.length > 0 ? 'flex' : 'none'
+
+        // first value is upload area
+        const renderValue = ['', ...this.imagesFiles]
+
+        imgUploadAreaPreview.innerHTML = renderValue
+            .map((image, index) => {
+                if (index === 0) {
+                    return `
+                    <label for="post__images" class="post__images--upload--area uploaded">
+                        <i class="fa-solid fa-plus"></i>
+                    </label>
+                `
+                }
+
+                return `
+                <div class="post__images--upload--area--preview__item">
+                    <img
+                        src="${image.preview}"
+                        alt=""
+                    />
+                    <button class="post__images--upload--area--preview__item__remove" data-index="${index - 1}">
+                        <i class="fa-solid fa-xmark"></i>
+                    </button>
+                </div>
+            `
+            })
+            .join('')
+    },
+
     handleEvent() {
         categoryBtnsOption.forEach((btn) => {
             btn.onclick = () => {
@@ -77,6 +114,37 @@ const postApp = {
                 this.roleType = btn.dataset.type
             }
         })
+
+        imgInput.onchange = (e) => {
+            const files = e.target.files
+
+            if (files?.length) {
+                Array.from(files).forEach((file, index) => {
+                    if (this.imagesFiles.length >= 12 || index >= 12) {
+                        return
+                    }
+
+                    file.preview = URL.createObjectURL(file)
+                    this.imagesFiles.push(file)
+                })
+            }
+
+            // allow user to upload multiple images same name
+            e.target.value = ''
+
+            this.handleLoadImagesPreview()
+        }
+
+        imgUploadAreaPreview.onclick = (e) => {
+            if (e.target.closest('.post__images--upload--area--preview__item__remove')) {
+                const btn = getParentElement(e.target, '.post__images--upload--area--preview__item__remove')
+
+                const imageIndex = btn.dataset.index
+
+                this.imagesFiles.splice(imageIndex, 1)
+                this.handleLoadImagesPreview()
+            }
+        }
     },
 
     init() {
