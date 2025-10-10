af06c7412e15913c1049c09917f7c36c6063ab6d
yenlinh685 <yenlinh060825@gmail.com>
2025-10-10 16:24:39 +0000

    feat: add comment section and user information display to post details

--- src/js/details.js
+++ src/js/details.js
@@ -17,10 +17,15 @@ const detailsUnitPrice = document.querySelector('.details__info--unit__price')
 const detailsAcreageValue = document.querySelector('.details__info--acreage__value')
 const detailsLocationValue = document.querySelector('.details__info--location__value')
 const detailsLocationUpdated = document.querySelector('.details__info--location__updated')
+const commentInput = document.querySelector('textarea[name="comment-input"]')
+const commentSubmit = document.querySelector('.comment--input__submit')
+const postComments = document.querySelector('.post__comment__wrapper__container')
 
 const postId = Number(getUrlSearchParams('post_id'))
-
 const currentPost = JSON.parse(localStorage.getItem('posts')).find((post) => post.id === postId)
+const postUser = JSON.parse(localStorage.getItem('users')).find((user) => user.id === currentPost.user_id)
+const comments = JSON.parse(localStorage.getItem('comments')) || []
+
 let activeImageIndex = 0
 
 // render images
@@ -202,11 +207,11 @@ function renderDetails() {
 
 function renderDescription() {
     document.querySelector('.details__info__description--content').textContent = currentPost.description
-    document.querySelector('.details__info__description--contact--value').textContent = currentPost.user.phone
+    document.querySelector('.details__info__description--contact--value').textContent = postUser.phone
     const copyBtn = document.querySelector('.details__info__description--contact--copy')
 
     copyBtn.addEventListener('click', () => {
-        navigator.clipboard.writeText(currentPost.user.phone)
+        navigator.clipboard.writeText(postUser.phone)
         copyBtn.textContent = 'Đã sao chép'
         setTimeout(() => {
             copyBtn.textContent = 'Sao chép'
@@ -214,10 +219,163 @@ function renderDescription() {
     })
 }
 
+function renderUserPost() {
+    const roleMapping = {
+        user: {
+            label: 'Cá nhân',
+            icon: '<i class="fa-regular fa-circle-user"></i>',
+        },
+        agent: {
+            label: 'Môi giới',
+            icon: '<i class="fa-solid fa-briefcase"></i>',
+        },
+        admin: {
+            label: 'Quản trị viên',
+            icon: '<i class="fa-solid fa-user-tie"></i>',
+        },
+    }
+
+    const postLength = JSON.parse(localStorage.getItem('posts')).filter((post) => {
+        return post.user_id === currentPost.user_id
+    }).length
+
+    document.querySelector('.details__user__info__wrapper').innerHTML = `
+        <div class="details__user__info__wrapper__header">
+            <img
+                src="${postUser.avatar}"
+                alt=""
+                onerror="this.src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8PyKYrBKAWWy6YCbQzWQcwIRqH8wYMPluIZiMpV1w0NYSbocTZz0ICWFkLcXhaMyvCwQ&usqp=CAU'"
+            />
+            <div class="details__user__info__wrapper__content">
+                <h3>${postUser.full_name}</h3>
+                <p class="details__user__info__wrapper__content--role">
+                    ${roleMapping[postUser.role].icon}
+                    <span>${roleMapping[postUser.role].label}</span>
+                </p>
+            </div>
+        </div>
+        <div class="details__user__info__wrapper__content--actions">
+            <span> ${postLength} tin đăng </span>
+            <span> ${momentTimezone(postUser.created_at).replace('trước', '')} trên Nhà Tốt </span>
+        </div>
+        <a 
+            href="tel:${postUser.phone}" 
+            class="button--primary details__user__info--contact--btn" 
+            target="_blank"
+            data-phone="${postUser.phone}"
+        >
+            <span><i class="fa-solid fa-phone-volume"></i></span>
+            <span>${postUser.phone || 'Không có số điện thoại'}</span>
+        </a>
+    `
+}
+
+const handleLoadComments = () => {
+    if (comments.length > 0) {
+        postComments.classList.add('has-comments')
+        handleRenderComment()
+    } else {
+        postComments.classList.remove('has-comments')
+    }
+}
+
+const handleRenderComment = () => {
+    document.querySelector('.comment--list').innerHTML = comments
+        .map((comment) => {
+            // if comment is a reply, skip
+            if (comment.parent_id) {
+                return null
+            }
+
+            const commentUser = JSON.parse(localStorage.getItem('users')).find((user) => user.id === comment.user_id)
+            const replyCount = JSON.parse(localStorage.getItem('comments')).filter(
+                (comment) => comment.parent_id === comment.id
+            ).length
+
+            return `
+                <div class="comment--item">
+                    <div class="comment--item__avatar">
+                        <img
+                            src="${commentUser.avatar}"
+                            alt="${commentUser.full_name}"
+                            onerror="this.src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8PyKYrBKAWWy6YCbQzWQcwIRqH8wYMPluIZiMpV1w0NYSbocTZz0ICWFkLcXhaMyvCwQ&usqp=CAU'"
+                        />
+                    </div>
+                    <div class="comment--item__content__wrapper">
+                        <div class="comment--item__content">
+                            <h5>${commentUser.full_name}</h5>
+                            <p>${comment.content}</p>
+                        </div>
+                        <div class="comment--item__meta">
+                            <span class="comment--item__time">${momentTimezone(comment.created_at).replace(
+                                'trước',
+                                ''
+                            )}</span>
+                            <div class="comment--item__actions">
+                                <button class="comment--item__action">
+                                    <i class="fa-regular fa-comment"></i> Trả lời
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <button class="reply--count ${replyCount > 0 ? 'active' : ''}">Xem ${replyCount} phản hồi</button>
+        `
+        })
+        .join('')
+}
+
+const handleComment = () => {
+    const value = commentInput.value
+
+    if (value.trim() === '') {
+        return
+    }
+
+    const comment = {
+        id: comments.length > 0 ? comments[comments.length - 1].id + 1 : 1,
+        post_id: postId,
+        content: value,
+        parent_id: null,
+        user_id: postUser.id,
+        created_at: new Date(),
+        updated_at: new Date(),
+    }
+
+    comments.push(comment)
+    localStorage.setItem('comments', JSON.stringify(comments))
+    handleRenderComment()
+}
+
+commentInput.oninput = (e) => {
+    if (e.target.value.split('\n').length === 1) {
+        commentInput.style.overflow = 'hidden'
+    } else {
+        commentInput.style.overflow = 'auto'
+    }
+
+    commentInput.style.height = 'auto'
+    commentInput.style.height = commentInput.scrollHeight + 'px'
+}
+
+commentInput.onkeydown = (e) => {
+    if (e.key === 'Enter') {
+        e.preventDefault()
+
+        handleComment()
+    }
+}
+
+commentSubmit.addEventListener('click', () => {
+    handleComment()
+})
+
 renderImages()
 handleLoadCurrentImage(activeImageIndex)
 loadPostDetails()
 renderDetails()
 renderDescription()
+handleLoadComments()
+renderUserPost()
 
 defaultApp.init()
