a8fb0d88b807e7b9c4397081be951b26afa6be37
yenlinh685 <yenlinh060825@gmail.com>
2025-10-30 16:14:39 +0000

    style: giao diện các bài post theo tab

--- src/js/user.js
+++ src/js/user.js
@@ -1,9 +1,21 @@
 import defaultApp from './default'
 
-const userApp = {
-    init() {
-        defaultApp.init()
-    },
-}
+const postManagerTabs = document.querySelectorAll('.post__manager__tabs--button')
 
-userApp.init()
+let activeTab = 'approved'
+
+postManagerTabs.forEach((tab) => {
+    tab.onclick = () => {
+        postManagerTabs.forEach((tab) => {
+            tab.classList.remove('active')
+        })
+
+        tab.classList.add('active')
+
+        activeTab = tab.dataset.type
+    }
+})
+
+console.log(activeTab)
+
+defaultApp.init()
