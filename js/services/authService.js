ab13145bfdbd1360654a68a9a59ae64cf4415a11
HuanCanhCut <tronghuanxxx@gmail.com>
2025-12-29 21:32:48 +0700

    feat: call api register

--- js/services/authService.js
+++ js/services/authService.js
@@ -9,3 +9,12 @@ export const login = async (email, password) => {
         throw error
     }
 }
+
+export const register = async (email, password) => {
+    try {
+        const response = await fetchClient.post('/api/auth/register', { email, password })
+        return response.json()
+    } catch (error) {
+        throw error
+    }
+}
